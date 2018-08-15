f4ee46369a650bb96ff761b8e347d9a184058ec5
r-hall <ryanhall2015@gmail.com>
2018-08-15 15:01:15 -0700

    refresh friends aws SQS

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Friends = require('./db.js').Friends;
 const Users = require('./db.js').Users;
 const getFriends = require('./friends.js');
 const addFriend = require('./addFriend.js');
+const refreshFriends = require('./config/awsURLs.js');
 const port = process.env.PORT || 3001;
 
 // Load the AWS SDK for Node.js
@@ -32,12 +33,8 @@ app.get('/health', (req, res) => {
 
 app.post('/friends/:friendId/:viewerId', async (req, res) => {
   try {
-    console.log('in post friends');
     let friendId = req.params.friendId;
     let viewerId = req.params.viewerId;
-    console.log('friendId in index', friendId);
-    console.log('viewerId in index', viewerId);
-    // refactor to a promise all
     let viewer = await Users.findOne({id: viewerId});
     let friends = await Friends.findOne({id: viewerId});
     let tokenKey = viewer.twitterTokenKey;
@@ -49,7 +46,6 @@ app.post('/friends/:friendId/:viewerId', async (req, res) => {
       access_token_secret: tokenSecret
     });
     let friend = await addFriend(client, friendId, viewerId, viewer.friends);
-    console.log('friend in index', friend);
     res.writeHead(201);
     res.end(friend);
   } catch(err) {
@@ -63,25 +59,19 @@ app.get('/friends/:userId/:viewerId', async (req, res) => {
   try {
     let userId = req.params.userId;
     let viewerId = req.params.viewerId;
-    console.log('userId in friendsAPI', userId);
-    console.log('viewerId in friendsAPI', viewerId);
     // used to refresh friends for userId, viewer's tokens are used
     let message = viewerId + '.' + userId;
     let user = await Friends.findOne({id: userId});
-    console.log('user from friendsDB', user);
     if (user && user.friends.length) {
       let friends = user.friends;
       let refreshedFriendsDate = user.refreshedFriendsDate;
-      console.log('refreshedFriendsDate', refreshedFriendsDate);
       let currentDate = new Date();
       let hours = Math.abs(refreshedFriendsDate - currentDate) / 36e5;
-      console.log('hours', hours);
       if (hours > 24) {
-        console.log('hours > 24, need to refresh friends');
         let params = {
           DelaySeconds: 10,
           MessageBody: message,
-          QueueUrl: "https://sqs.us-east-1.amazonaws.com/292328237082/refresh-friends"
+          QueueUrl: refreshFriends
          };
          sqs.sendMessage(params, function(err, data) {
            if (err) {
@@ -98,7 +88,6 @@ app.get('/friends/:userId/:viewerId', async (req, res) => {
       }
     } else {
       let viewer = await Users.findOne({id: viewerId})
-      console.log('viewer object in friendsAPI', viewer);
       let tokenKey = viewer.twitterTokenKey;
       let tokenSecret = viewer.twitterTokenSecret;
       let client = new Twitter({
@@ -107,11 +96,10 @@ app.get('/friends/:userId/:viewerId', async (req, res) => {
         access_token_key: tokenKey,
         access_token_secret: tokenSecret
       });
-      console.log("friends weren't found in db, need to refresh for first time");
       let params = {
         DelaySeconds: 10,
         MessageBody: message,
-        QueueUrl: "https://sqs.us-east-1.amazonaws.com/292328237082/refresh-friends"
+        QueueUrl: refreshFriends
        };
        sqs.sendMessage(params, function(err, data) {
          if (err) {
@@ -131,7 +119,6 @@ app.get('/friends/:userId/:viewerId', async (req, res) => {
   }
 })
 
-// Launch the server
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 })
